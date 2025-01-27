3c4df818504a24069571dc414bbfee36250920d8
Henok <henokassefa494@gmail.com>
2025-01-26 22:13:02 -0500

    "Task 2 - Product Details."

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,11 @@ var isActive = true
 console.log(employeeName, employeeID, isActive)
 typeof("Ben")
 typeof(2003)
+
+// Task 2: Arithmetic Operations
+let productName = "Charger"
+const productPrice = 5
+var isAvailable = true
+console.log(productName, productPrice, isAVailable)
+tyoeof("Charger")
+typeof(5)
