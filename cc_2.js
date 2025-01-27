d9e6e9eb145de2b10eb4e559d93c3dfaacb88d0d
Henok <henokassefa494@gmail.com>
2025-01-26 22:58:54 -0500

    "Task 1 - Store Inventory."

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+// Task 1: Working with Arrays
+let products = ("Brush", "Comb", "Pick", "Oil", "Gel")
+let products = ("Brush", "Comb", "Pick", "Oil", "Gel", "Cream")
+let products = ("Brush", "Comb", "Pick", "Oil", "Gel")
+console.log("Brush", "Comb", "Pick", "Oil", "Gel")
