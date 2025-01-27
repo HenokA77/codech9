abad11bfaad1f48a395e66b7156844f7b091059a
Henok <henokassefa494@gmail.com>
2025-01-26 23:07:24 -0500

    "Task 2 - Student Scores."

--- cc_2.js
+++ cc_2.js
@@ -3,3 +3,12 @@ let products = ("Brush", "Comb", "Pick", "Oil", "Gel")
 let products = ("Brush", "Comb", "Pick", "Oil", "Gel", "Cream")
 let products = ("Brush", "Comb", "Pick", "Oil", "Gel")
 console.log("Brush", "Comb", "Pick", "Oil", "Gel")
+
+// Task 2 Accessing and Modifying Arrays
+let scores = ( 92, 93, 91, 95, 98)
+let scores = ( 92, 94, 91 95, 98)
+((92+94+91+95+98)/5)
+console.log(92, 94, 91, 95, 98, 94)
+
+
+
