6a08eeaddb1ae76cfb4153ebeb35c7a6c7742b66
Henok <henokassefa494@gmail.com>
2025-02-02 20:03:53 -0500

    Task 5 - Inventory Management System

--- cc_3.js
+++ cc_3.js
@@ -18,3 +18,10 @@ console.log(employee);
 let feedback = [{customerName: "Alice", feedbackText: "Great product, very satisfied!", rating: 5},{customerName: "Bob", feedbackText: "Decent quality, but could be improved.", rating: 3}, {customerName: "Charlie", feedbackText: "Excellent service, will buy again.", rating: 4}];
 feedback.push({customerName: "David", feedbackText: "Not satisfied with the product, it broke quickly.", rating: 2});
 console.log(feedback);
+
+// Task 5
+let inventory  = {itemName: "Laptop", stockCount: 25, price: 799.99};
+let inventory  = {itemName: "Laptop", stockCount: 25, price: 799.99, calculateTotalValue: function() {return this.stockCount * this.price;}};
+console.log(inventory);
+console.log("Total value of inventory:", inventory.calculateTotalValue()):
+
