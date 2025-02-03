3ca2767a75f3cfaec1ee78fd2f420605c8b5ec70
Henok <henokassefa494@gmail.com>
2025-02-02 19:17:11 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -1,3 +1,9 @@
 let prices = [10, 25, 7, 15, 42];
 prices.push(30);
 prices.shift();
+
+// Task 2
+let orders = [5,12,8,20,15];
+orders[2] += 5;
+let totalOrders = orders.reduce((sum, order) => sum + order, 0);
+console.log(totalOrders);
