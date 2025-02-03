db1195fab42d0376535d05088b500cdb84a8ba2c
Henok <henokassefa494@gmail.com>
2025-02-02 21:27:01 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -4,3 +4,9 @@ let discount = 0;
 if (purchaseAmount > 100) {discount = 0.1; purchaseAmount = purchaseAmount - (purchaseAmount * discount);}
 console.log("Total after discount: $" + purchaseAmount.toFixed(2));
 
+// Task 2 
+let sales = [1200, 900, 1500, 800, 1000];
+let totalSales = 0;
+for (let i = 0; i < sales.length; i++) {totalSales +- sales[i];}
+console.log("Total sales: $" + totalSales.toFixed(2));
+
