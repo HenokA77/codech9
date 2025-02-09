102ffa7f579bb328d24ddeb040693ef82c8e988c
Henok <henokassefa494@gmail.com>
2025-02-09 15:21:56 -0500

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,13 @@
+// Task 1
+// Declare the customer object
+const customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+
+// Log each property using template literals
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
+
