8e03d86f2e303db3a7cf605937bc7eb95fc900ca
Henok <henokassefa494@gmail.com>
2025-02-16 23:31:00 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -8,3 +8,14 @@ let netSalary = calculateSalary(50000, 5000, 0.2);
 console.log(`Net Salary: $${netSalary.toFixed(2)}`);
 calculateSalary(5000, 500, 0.1); // Expected output: "Net Salary: $5000.00"
 calculateSalary(7000, 1000, 0.15); // Expected output: "Net Salary: $6950.00"
+
+// Task 2
+const calculateDiscount = function(price, discountRate) {
+    return price - (price * discountRate);
+};
+
+// Example usage:
+let finalPrice = calculateDiscount(100, 0.2);
+console.log(`Final Price: $${finalPrice.toFixed(2)}`);
+calculateDiscount(100, 0.2); // Expected output: "Final Price: $80.00"
+calculateDiscount(250, 0.15); // Expected output: "Final Price: $212.50"
